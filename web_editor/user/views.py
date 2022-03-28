7831735a9244459f27bc17d3a2784df5060cd965
hansmaan <gkscldfl321@naver.com>
2022-03-28 10:43:59 +0900

    csrf token 반영

--- web_editor/user/views.py
+++ web_editor/user/views.py
@@ -4,19 +4,20 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from django.contrib.auth import authenticate, login
 from rest_framework.views import APIView
+from rest_framework.viewsets import GenericViewSet
 
-from .serializer import UserCreateSerializer
+from .serializer import *
 from .models import User
-from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
 
-class UserCreateViewSet(APIView):
-    def post(self, request, *args, **kwargs):
-        serializer = UserCreateSerializer(data=request.data)
+class UserCreateViewSet(GenericViewSet):
+    serializer_class = UserCreateSerializer
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
         try:
             serializer.is_valid(raise_exception=True)
             user = serializer.save()
-            return Response(data = {'user' : UserCreateSerializer(user).data}, status=status.HTTP_201_CREATED)
+            return Response(data = {'user' : self.get_serializer(user).data}, status=status.HTTP_201_CREATED)
         except ValidationError as e:
             return Response(data=e, status=400)
 
@@ -25,6 +26,7 @@ class UserLoginViewSet(APIView):
         user_id = request.data.get('user_id')
         password = request.data.get('password')
         user = authenticate(request, username=user_id, password=password)
-        if user == None:
-            return Response(data = {'error' : '아이디나 비밀번호가 잘못되었습니다.'}, status=status.HTTP_404_NOT_FOUND)
-        return Response(data = {'login' : True}, status=status.HTTP_200_OK)
+        if user:
+            login(request, user)
+            return Response(UserSerializer(user).data)
+        return Response(status=status.HTTP_403_FORBIDDEN, data='아이디나 패스워드가 일치하지 않습니다.')
