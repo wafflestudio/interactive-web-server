e8ecbe2073076443516755649cc8a31c86b515e1
padme0421 <padme0421@gmail.com>
2022-04-23 08:23:06 +0900

    fix: copy secret.json into deploy

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,7 @@
 cd /home/ec2-user/deploy
 git clone https://github.com/wafflestudio/interactive-web-server.git
 cd interactive-web-server
+cp /home/ec2-user/deploy/secret.json web_editor
 sudo chmod +x init-letsencrypt.sh
 sudo ./init-letsencrypt.sh
 docker-compose down
