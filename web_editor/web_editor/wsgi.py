c84258181cbe2be6a9a8a4322354f543b0b106d3
padme0421 <padme0421@gmail.com>
2022-04-22 19:41:29 +0900

    fix: modify specified location of settings

--- web_editor/web_editor/wsgi.py
+++ web_editor/web_editor/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings') # changed from web_editor.settings to settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getcwd()+'/settings') # changed from web_editor.settings to settings
 
 application = get_wsgi_application()
