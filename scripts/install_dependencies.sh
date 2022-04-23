dd81ab66527d90f8bb2927923eb2b4051d992370
padme0421 <padme0421@gmail.com>
2022-04-23 14:34:54 +0900

    fix: comment out ssl (temp)

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,5 +3,5 @@ git clone https://github.com/wafflestudio/interactive-web-server.git
 cd interactive-web-server
 cp /home/ec2-user/deploy/secret.json /home/ec2-user/deploy/interactive-web-server/web_editor/
 sudo chmod +x init-letsencrypt.sh
-sudo ./init-letsencrypt.sh
+./init-letsencrypt.sh
 docker-compose down
