68be2ae8b8409256d86a29f3a7a873f86b194437
padme0421 <padme0421@gmail.com>
2022-04-23 14:09:46 +0900

    fix: change proxy pass to iwe-server.shop, remove docker-compose command check

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v docker-compose)" ]; then
-  echo 'Error: docker-compose is not installed.' >&2
-  exit 1
-fi
+#if ! [ -x "$(command -v docker-compose)" ]; then
+  #echo 'Error: docker-compose is not installed.' >&2
+  #exit 1
+#fi
 
 domains=(iwe-server.shop) #www.iwe-server.shop
 rsa_key_size=4096
