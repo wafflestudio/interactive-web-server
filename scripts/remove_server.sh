743375377ae7facdcf66342b57b5cda2c49e8748
padme0421 <padme0421@gmail.com>
2022-04-24 21:21:42 +0900

    fix: add clean up jobs, add db host to ci

--- scripts/remove_server.sh
+++ scripts/remove_server.sh
@@ -3,6 +3,8 @@ if [ -d "interactive-web-server" ]
 then 
     cd ./interactive-web-server
     docker-compose down
+    docker system prune
+    docker builder prune
     cd /home/ec2-user/deploy
     rm -rf ./interactive-web-server
 fi
