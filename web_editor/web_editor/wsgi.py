5e452dd4618cf559daa7724626e0e588d4661d62
padme0421 <padme0421@gmail.com>
2022-07-06 22:16:24 +0900

    feat: add settings for socketio

--- web_editor/web_editor/wsgi.py
+++ web_editor/web_editor/wsgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+import settings
 
 from django.core.wsgi import get_wsgi_application
 
+import socketio
+import eventlet
+import eventlet.wsgi
+
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_editor.settings')
 
-application = get_wsgi_application()
+django_app = get_wsgi_application() # application
+sio = socketio.Server(async_mode='eventlet', cors_allowed_origins=settings.CORS_ALLOWED_ORIGINS, cors_credentials=True)
+# check validity of cors_allowed_origins = ... code
+application = socketio.WSGIApp(sio, django_app)
+eventlet.wsgi.server(eventlet.listen(('', 8000)), application)
