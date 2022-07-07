d4a584e5f01f2e9ecd211d0c4973d08f9a1f5bce
padme0421 <padme0421@gmail.com>
2022-07-07 23:00:55 +0900

    fix: add ws/ to uri

--- web_editor/project/routing.py
+++ web_editor/project/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('project/<str:pk>/', consumers.ProjectConsumer.as_asgi()),
+    path('/ws/project/<str:pk>/', consumers.ProjectConsumer.as_asgi()),
 ]
