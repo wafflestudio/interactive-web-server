7480119cc0d4e1d029722924054ee7e71095a218
padme0421 <padme0421@gmail.com>
2022-07-26 23:14:47 +0900

    fix: asgi config

--- web_editor/web_editor/asgi.py
+++ web_editor/web_editor/asgi.py
@@ -12,14 +12,13 @@ import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_editor.settings')
 
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 
 import project.routing
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
   "http": django_asgi_app,
