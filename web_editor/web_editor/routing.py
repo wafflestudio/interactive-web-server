3467b4d33c2d1892c170547ea5d3ecaeb058173b
padme0421 <padme0421@gmail.com>
2022-07-27 22:10:16 +0900

    feature: uwsgi config

--- web_editor/web_editor/routing.py
+++ web_editor/web_editor/routing.py
@@ -3,6 +3,8 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import project.routing
 
+ASGI_APPLICATION = "web_editor.routing.application"
+
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
     'websocket': AuthMiddlewareStack(
