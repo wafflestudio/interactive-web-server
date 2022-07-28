11301b45ceb31508803d84d4ed4077dae2a2dd38
hansmaan <gkscldfl321@naver.com>
2022-07-28 21:20:09 +0900

    feat: s3 setting

--- web_editor/common/common.py
+++ web_editor/common/common.py
@@ -1,5 +1,7 @@
 from django.db import models
-
+import datetime
+import os
+import uuid
 
 class CustomModelManager(models.Manager):
     def get_or_none(self, **kwargs):
@@ -7,3 +9,11 @@ class CustomModelManager(models.Manager):
             return self.get(**kwargs)
         except self.model.DoesNotExist:
             return None
+
+def file_upload_path(instance, filename):
+    ext = filename.split('.')[-1]
+    d = datetime.datetime.now()
+    filepath = d.strftime("%Y/%m/%d")
+    suffix = d.strftime("%Y%m%d%H%M%S")
+    filename = "%s_%s_%s" % (uuid.uuid4().hex, suffix, ext)
+    return os.path.join(filepath, filename)
