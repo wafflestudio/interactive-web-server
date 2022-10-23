d1b88e5bf3f8ea264b2479292ffe067d645388c2
padme0421 <padme0421@gmail.com>
2022-10-23 16:17:41 +0900

    shorten access token lifetime

--- web_editor/web_editor/settings.py
+++ web_editor/web_editor/settings.py
@@ -190,7 +190,7 @@ REST_FRAMEWORK = {
 REST_USE_JWT = True
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=20),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': False,
