49390cd9f7210d63ae5be9125ea8d7264fd2b202
padme0421 <padme0421@gmail.com>
2022-11-12 12:08:42 +0900

    fix prod aws settings

--- web_editor/web_editor/settings/prod.py
+++ web_editor/web_editor/settings/prod.py
@@ -9,11 +9,13 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_ACCESS_KEY_ID = get_secret("AWS_ACCESS_KEY")
 AWS_SECRET_ACCESS_KEY = get_secret("AWS_SECRET_KEY")
-AWS_REGION = get_secret("AWS_REGION")
+AWS_S3_REGION_NAME = get_secret("AWS_S3_REGION_NAME")
 
 AWS_STORAGE_BUCKET_NAME = get_secret("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (
-    AWS_STORAGE_BUCKET_NAME, AWS_REGION)
+#AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (
+    #AWS_STORAGE_BUCKET_NAME, AWS_REGION)
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
+
+AWS_S3_SIGNATURE_VERSION = 's3v4'
